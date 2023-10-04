d20bdbaf866584ea6be09b4e14c4680bd3c6c988
Lucas Danilo <lukastecno@gmail.com>
2023-10-04 14:06:30 -0300

    Product

--- src/main/java/com/projetospring/course/repositories/CategoryRepository.java
+++ src/main/java/com/projetospring/course/repositories/CategoryRepository.java
@@ -2,7 +2,7 @@ package com.projetospring.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.projetospring.course.entities.enums.Category;
+import com.projetospring.course.entities.Category;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
--- src/main/java/com/projetospring/course/services/CategoryService.java
+++ src/main/java/com/projetospring/course/services/CategoryService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.projetospring.course.entities.enums.Category;
+import com.projetospring.course.entities.Category;
 import com.projetospring.course.repositories.CategoryRepository;
 
 @Service
